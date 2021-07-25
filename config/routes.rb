e3f7709ff56efa0395b5d33d06c2b2a3ac54242b
Vivian stanley <vivianstanley@Vivians-MacBook-Air.local>
2021-07-25 00:51:50 -0700

    commit tree 353ed6e3172e3930438ab4a5c52d89551c75e923

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/secret" => 'secrets#show'
+  get "/login" => 'sessions#new'
+  post "/logout" => 'sessions#destroy'
+  post "/login" => 'sessions#create'
+  root 'application#index'
 end
